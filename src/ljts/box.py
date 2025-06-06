dec00d94608ccda48c36b925af2f041f2fb997b7
fredericstrand <strandfrederic@outlook.com>
2025-06-06 18:48:26 +0200

    fixed some issues the teacher raised

--- src/ljts/box.py
+++ src/ljts/box.py
@@ -1,13 +1,12 @@
 import numpy as np
 from src.ljts.molecule import Molecule
 
-
 class Box:
-    def __init__(self, lx, ly, lz, den_liq=None, den_vap=None):
+    def __init__(self, len_x, len_y, len_z, den_liq=None, den_vap=None):
         """
         initializing the box class that will contain all the molecules. and will also generate the distrobution
         """
-        self._size = np.array([lx, ly, lz])
+        self._size = np.array([len_x, len_y, len_z])
         self._molecules = []
         self._total_Epot = 0
 
@@ -37,28 +36,23 @@ class Box:
         """
         function for populating the box/system by taking the parameters defined in main script and from that creating a distrobution of molecules within the different zones
         """
-        lx, ly, lz = self._size
-        vol = lx * ly * lz
+        len_x, len_y, len_z = self._size
+        vol = len_x * len_y * len_z
 
         ratios = np.array([2, 1, 2])
         fractions = ratios / ratios.sum()
-        boundaries = np.cumsum(np.insert(fractions * ly, 0, 0))
+        boundaries = np.cumsum(np.insert(fractions * len_y, 0, 0))
         zone = fractions * vol
         densities = [den_vap, den_liq, den_vap]
         mol_count = (densities * zone).astype(int)
 
         for (max, min), count in zip(zip(boundaries[:-1], boundaries[1:]), mol_count):
-            pos = np.random.uniform(
-                low=(0, min, 0), high=(lx, max, lz), size=(count, 3)
-            )
-            self._molecules.append(map(Molecule, pos))
+            pos = np.random.uniform(low=(0, min, 0), high=(len_x, max, len_z), size=(count, 3))
+            self._molecules.extend([Molecule(pos) for pos in pos])
 
     """ 
     defining the getters, no need for setters
     """
-
-    """     def get_size(self):
-        return self._size """
 
     @property
     def get_molecules(self):
--- src/ljts/molecule.py
+++ src/ljts/molecule.py
@@ -1,16 +1,15 @@
 import numpy as np
 
-""" 
-defining variables outside of loop to save computational power as these will not change
-"""
-u6 = 2.5**6
-u12 = u6**2
-u = 4 * ((1.0 / u12) - (1.0 / u6))
-
-
 class Molecule:
     def __init__(self, position):
-        self._q = np.array(position)
+        if len(position) != 3:
+            raise ValueError("Position must be a 3D vector with three components.")
+
+        self._x = position[0]
+        self._y = position[1]
+        self._z = position[2]
+        self._position = np.array(position)
+        self._u = 4 * ((1.0 / 2.5**12) - (1.0 / 2.5**6))
 
     def kinetic_energy(self):
         return 0.5 * np.dot(self.v, self.v)
@@ -19,7 +18,7 @@ class Molecule:
         """
         potential energy for molecule and each neighbor given formula from task
         """
-        delta = self._q - other._q
+        delta = self._position - other._position
 
         delta -= size * np.round(delta / size)
 
@@ -27,7 +26,8 @@ class Molecule:
 
         if r2 >= 6.25:
             return 0.0
+
         inv_r2 = 1.0 / r2
         inv_r6 = inv_r2**3
         inv_r12 = inv_r6**2
-        return 4 * (inv_r12 - inv_r6) - u
+        return 4 * (inv_r12 - inv_r6) - self._u
