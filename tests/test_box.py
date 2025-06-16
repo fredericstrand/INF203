58ef765449af33236f5ab248dc0fa5340d6990cb
Christopher Ljosland Strand <127755148+ChristopherStrand@users.noreply.github.com>
2025-06-16 18:42:50 +0200

    Test/validation (#7)
    * no
    
    * feat: started on molecule tests
    
    * feat: implemented tests for box class
    
    * feat: finished testing

---
+++ tests/test_box.py
@@ -0,0 +1,37 @@
+from ljts.box import Box
+from ljts.potential import LJTS
+import pytest
+import numpy as np
+
+
+def test_box_size():
+    box = Box(10.0, 10.0, 10.0, potential=LJTS())
+    assert np.allclose(
+        box.box_size, [10.0, 10.0, 10.0]
+    ), "the length in each dimension does not match "
+
+
+def test_volume():
+    box = Box(10.0, 10.0, 10.0, potential=LJTS())
+    assert np.prod(box.box_size) == pytest.approx(
+        1000.0
+    ), "the volume of the box does not match given the dimensions"
+
+
+def test_populate():
+    box = Box(5.0, 40.0, 5.0, potential=LJTS())
+    box.populate_box(den_liq=0.73, den_vap=0.02)
+    assert (
+        len(box._molecules) == 162
+    ), "input num molecules does not match the real num molecules"
+
+
+def test_inside_box():
+    box = Box(10.0, 10.0, 10.0, potential=LJTS())
+    box.populate_box(den_liq=0.73, den_vap=0.02)
+    for mol in box._molecules:
+        mol.move_random(0.5, 8.0)
+    assert all(
+        np.all((mol.alt_position >= 0) & (mol.alt_position < 8.0))
+        for mol in box._molecules
+    ), "at least one molecule is outside of the box"
---
+++ tests/test_molecule.py
@@ -0,0 +1,43 @@
+from ljts.molecule import Molecule
+import numpy as np
+import pytest
+
+
+def test_position():
+    mol = Molecule([1.0, 2.0, 3.0])
+    assert np.allclose(
+        mol.position, [1.0, 2.0, 3.0]
+    ), "position given to constructor is not molecules position"
+
+
+@pytest.mark.parametrize(
+    "invalid_position", [[1.0, 2.0], [1.0], [], [1.0, 2.0, 3.0, 4.0]]
+)
+def test_position_dimension(invalid_position):
+    with pytest.raises(ValueError, match="must be 3D"):
+        Molecule(invalid_position)
+
+
+def test_move_random():
+    mol = Molecule([3.0, 2.0, 1.0])
+    box_size = 10
+    b = 0.5
+    mol.move_random(b, box_size)
+    delta = (mol.alt_position - mol.position + box_size / 2) % box_size - box_size / 2
+    assert np.all(np.abs(delta) <= b), "the molecule is moved outside of the box"
+
+
+def test_getter_position():
+    mol = Molecule([4.0, 5.0, 6.0])
+    assert np.allclose(mol.position, [4.0, 5.0, 6.0])
+
+
+def test_getter_alt_position():
+    mol = Molecule([7.0, 8.0, 9.0])
+    assert np.allclose(mol.alt_position, [7.0, 8.0, 9.0])
+
+
+def test_setter_position():
+    mol = Molecule([0.0, 0.0, 0.0])
+    mol.position = [7.0, 8.0, 9.0]
+    assert np.allclose(mol.position, [7.0, 8.0, 9.0])
